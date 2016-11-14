603510b3855c08cc58485b0752120a5dca3fedca
flxzheyekeji <603479018@qq.com>
2016-11-14 23:57:58 +0800

    jiegou

--- js/controllers/totle.js
+++ js/controllers/totle.js
@@ -4,12 +4,6 @@
 define(function(require,expotrs,module){
      var contModule = require('js/controllers/base');
     contModule.registerController('totleController', ["$scope",function($scope){
-        $scope.tot = {
-            name:"jili",
-            class:'yaogan',
-            en:"50",
-            cn:'60',
-            math:'80'
-        }
+
     }]);
 });
